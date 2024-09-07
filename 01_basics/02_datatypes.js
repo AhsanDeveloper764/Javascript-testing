ccc6790a856c0a89f5bf8376eb16c3c5311168fc
Ahsan Developer <156519491+AhsanDeveloper764@users.noreply.github.com>
2024-09-07 22:04:09 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,14 @@
+/*
+number power 2 to 53
+biginit
+string  ""
+Boolean ===> true//false
+null ==> standalone value null ke type object hoti hay 
+undefined ==>
+Symbol ==> object
+object
+
+*/
+
+console.log(typeof undefined); ///undefined
+console.log(typeof null);   ///object
