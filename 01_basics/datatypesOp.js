25e70f12176d8a767abc385c8dd34e2d33bf0e8e
Ahsan Developer <156519491+AhsanDeveloper764@users.noreply.github.com>
2024-09-07 22:00:36 +0000

    Data_Types_Conversion

---
+++ 01_basics/datatypesOp.js
@@ -0,0 +1,93 @@
+// let score = "33"
+// console.log(typeof score)
+// console.log(typeof(score))
+
+// let valueInNumber = Number(score)
+// console.log(typeof(valueInNumber))
+// console.log(valueInNumber)
+
+// let score = "33abc"
+// console.log(typeof score)
+// console.log(typeof(score))
+// let valueInNumber = Number(score)
+// console.log(typeof(valueInNumber))
+// console.log(valueInNumber)
+
+
+// let score = "null"
+// console.log(typeof score)
+// console.log(typeof(score))
+// let valueInNumber = Number(score)
+// console.log(typeof(valueInNumber))
+// console.log(valueInNumber)
+
+// let score = null
+// console.log(typeof score)
+// console.log(typeof(score))
+// let valueInNumber = Number(score)
+// console.log(typeof(valueInNumber))
+// console.log(valueInNumber)
+
+
+//null is always object datatype
+// jab koi data type convert nhi hoti hy tw 0 ya NaN show hota hy output ma
+
+// let score = undefined
+// console.log(typeof score)
+// console.log(typeof(score))
+// let valueInNumber = Number(score)
+// console.log(typeof(valueInNumber))
+// console.log(valueInNumber)
+
+// let score = true
+// console.log(typeof score)
+// console.log(typeof(score))
+// let valueInNumber = Number(score)
+// console.log(typeof(valueInNumber))
+// console.log(valueInNumber)
+
+// let score = false
+// console.log(typeof score)
+// console.log(typeof(score))
+// let valueInNumber = Number(score)
+// console.log(typeof(valueInNumber))
+// console.log(valueInNumber)
+
+
+// let score = "Ahsan_ALi"
+// console.log(typeof score)
+// console.log(typeof(score))
+// let valueInNumber = Number(score)
+// console.log(typeof(valueInNumber))
+// console.log(valueInNumber)
+
+
+// notes
+/*
+33 is easily convert into other
+33abc is not convrt into other and shown a output NaN ===>>> is Not a number
+true is convert into 1 and flase is convert into 0
+*/
+
+// let isloogedIn = 1
+// let booleanIsLoggedIn  = Boolean(isloogedIn)
+// console.log(booleanisloogedIn)
+
+// let isloogedIn = 0
+// let BooleanIsLoggedIn  = Boolean(isloogedIn)
+// console.log(isloogedIn)
+
+// let isloogedIn = "Ahsan_Ali"
+// let booleanIsLoggedIn  = Boolean(isloogedIn)
+// console.log(booleanIsLoggedIn)     =====> true
+
+
+// let isloogedIn = ""
+// let booleanIsLoggedIn  = Boolean(isloogedIn)
+// console.log(booleanIsLoggedIn)     ======> False
+
+
+// let fav_number = 55
+// let stringIsNumber = String(fav_number)
+// console.log(stringIsNumber) 
+// console.log(typeof stringIsNumber) number data type is easily convert into string
