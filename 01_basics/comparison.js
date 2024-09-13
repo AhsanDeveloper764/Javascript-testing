b2554d693f650be27fb056d7064682f92f302116
Ahsan Developer <156519491+AhsanDeveloper764@users.noreply.github.com>
2024-09-13 21:00:23 +0000

    comparison of data types

---
+++ 01_basics/comparison.js
@@ -0,0 +1,16 @@
+// console.log("2">1);
+// console.log(1<"2");
+
+console.log(null > 0);
+console.log(null < 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined > 0);
+console.log(undefined < 0);
+console.log(undefined == 0);
+console.log(undefined >= 0);
+
+
+///is type kee compariosn hum nhi krtay hein logic building ma because code kee readability kharab hoti hy or confusion create
+//hoti hay
